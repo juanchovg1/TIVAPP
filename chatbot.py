ddff76b646a6f6553f5579bd12bbbc5b7f10935e
juanchovg1 <111707267+juanchovg1@users.noreply.github.com>
2023-09-18 00:08:13 -0500

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -1,1 +1,29 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+from typing import Optional
 
+app = FastAPI()
+app.title = "Documentación API1 TIVAPP"
+app.version = "0.0.1"
+
+
+class Libro(BaseModel):
+    titulo: str
+    autor: str
+    paginas: int
+    editorial: str
+
+
+@app.get("/")
+def index():
+    return {'message': 'hola mundo'}
+
+
+@app.get("/libros/{id}")
+def mostrar_libro(id: int):
+    return {"data": id}
+
+
+@app.post("/libros")
+def insertar_libro(libro: Libro):
+    return {"message": f"libro {libro.titulo} insertado"}
